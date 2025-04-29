f96900bb9a29789db31ec246c7cc7b0ed34cf117
arisdanang <arisdanang2810@gmail.com>
2025-04-29 22:58:33 +0700

    feat: add Home component

---
+++ src/ui/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+        The Best Pizza <br />
+        <span className="text-yellow-500">best pizza ever</span>
+      </h1>
+      <h2>continue order, Danang</h2>
+    </div>
+  );
+};
+
+export default Home;
