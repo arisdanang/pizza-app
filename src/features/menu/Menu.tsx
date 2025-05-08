01a778f85054ec869ba0e2521af8a7dfe879d850
arisdanang <arisdanang2810@gmail.com>
2025-05-08 22:40:25 +0700

    feat: consume data from menu

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,5 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import { MenuLoaderResponse } from "./menu.loader";
+
 const Menu = () => {
-  return <div>ini halaman Menu</div>;
+  const menu = useLoaderData() as MenuLoaderResponse;
+
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <li key={pizza.id}>p</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Menu;
