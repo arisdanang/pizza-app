c8c9f883bc85c7f6bf6692b12992a78f1c6bda13
arisdanang <arisdanang2810@gmail.com>
2025-05-12 07:33:35 +0700

    feat: seperate component into CartOverview

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
 
 const AppLayout = () => {
   const navigation = useNavigation();
@@ -15,13 +16,7 @@ const AppLayout = () => {
           <Outlet />
         </main>
       </div>
-      <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
-        <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-          <span>1 pizza</span>
-          <span>Rp. 12,000</span>
-        </p>
-        <div>open cart</div>
-      </div>
+      <CartOverview />
     </div>
   );
 };
